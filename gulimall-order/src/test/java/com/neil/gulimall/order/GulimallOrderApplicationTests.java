012d6c273da5045135f322f8afac60ff746b332e
Neil <neilcanyouhearme@gmail.com>
2022-02-13 14:47:19 +0800

    项目初始环境构建

---
+++ gulimall-order/src/test/java/com/neil/gulimall/order/GulimallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neil.gulimall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
